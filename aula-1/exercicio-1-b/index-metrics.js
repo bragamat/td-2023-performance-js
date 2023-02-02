8889fe16b43969aa78ec8ebb198576c8d188d653
bragamat <mateuscbraga@gmail.com>
2023-02-02 09:35:43 -0300

    exercicios

--- aula-1/exercicio-1-b/index-metrics.js
+++ aula-1/exercicio-1-b/index-metrics.js
@@ -1,11 +1,19 @@
 import express from 'express'
 
+// import { monitorEventLoopDelay } from 'perf_hooks'
+
+// const monitor = monitorEventLoopDelay()
+// monitor.enable()
+
 const APP = express()
 const PORT = 3000
 const LISTENER = () => console.log('Listening on PORT: ', PORT)
 
 APP.listen(3000, LISTENER)
 
+
 APP.get('/home', (req, res) => {
+  for(let n=0; n<1e9; n++) {}
+  // console.log(monitor.mean, monitor.max)
   res.send('hello world')
 })
